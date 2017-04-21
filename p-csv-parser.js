99c51683d29dc9e955fcb615696b49a5f6483b87
Dominik Winter <dominik.winter92@gmail.com>
2017-04-21 11:44:59 +0200

    (Add) comments

--- p-csv-parser.js
+++ p-csv-parser.js
@@ -7,30 +7,34 @@
   };
 
   /**
+   * p-csv-parser
+   * A simple and blazing fast csv parser, promisified.
    * 
-   * @param {*} str 
-   * @param {*} options 
+   * @param {String} csvStr The csv content as string.
+   * @param {Object} options An object containing the options.
    */
-  const csvParser = (str, options = {}) => {
+  const csvParser = (csvStr, options = {}) => {
     options = Object.assign(defaultOptions, options);
 
     return new Promise((resolve, reject) => {
-      let rows      = str.split('\n'),
+      let rows      = csvStr.split('\n'),
         headers     = (options.headers === true) ? rows.splice(0, 1)[0].split(options.delimiter) : options.headers,
         i           = 0,
         len         = rows.length,
-        headersLen = headers.length, 
+        headersLen  = headers.length, 
         results     = [],
         obj         = {},
         row, j;
 
-      for (; i < len; i++) {// Skip emty lines
+      for (; i < len; i++) {
+        // Skip emty lines
         if (rows[i].length === 0) continue;
         
         row = rows[i].split(options.delimiter);
         j   = 0;
 
         for (; j < headersLen; j++) {
+          // Set empty fields to null
           if (row[j].length === 0) obj[headers[j]] = null;
           else obj[headers[j]] = row[j];
         }
